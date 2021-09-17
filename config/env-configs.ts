1e131eb03518dfcc84327baf1c0e3d3d36c8acfd
Filip Pietruszyński <filippietruszynski@protonmail.com>
2021-09-17 23:53:26 +0200

    Rename NOT PROVIDED to NOT FOUND

--- config/env-configs.ts
+++ config/env-configs.ts
@@ -3,17 +3,17 @@ import { IEnvConfig, IProcessEnv } from "./types";
 const devConfig = (env: IProcessEnv): IEnvConfig => {
   return {
     app: {
-      host: env.DEV_APP_HOST || "NOT PROVIDED",
+      host: env.DEV_APP_HOST || "NOT FOUND",
       port: (env.DEV_APP_PORT && parseInt(env.DEV_APP_PORT, 10)) || 404,
     },
     database: {
-      host: env.DEV_DB_HOST || "NOT PROVIDED",
+      host: env.DEV_DB_HOST || "NOT FOUND",
       port: (env.DEV_DB_PORT && parseInt(env.DEV_DB_PORT, 10)) || 404,
-      name: env.DEV_DB_NAME || "NOT PROVIDED",
+      name: env.DEV_DB_NAME || "NOT FOUND",
     },
     secrets: {
-      token: env.DEV_TOKEN_SECRET || "NOT PROVIDED",
-      tokenExp: env.DEV_TOKEN_EXP || "NOT PROVIDED",
+      token: env.DEV_TOKEN_SECRET || "NOT FOUND",
+      tokenExp: env.DEV_TOKEN_EXP || "NOT FOUND",
     },
   };
 };
@@ -21,17 +21,17 @@ const devConfig = (env: IProcessEnv): IEnvConfig => {
 const testConfig = (env: IProcessEnv): IEnvConfig => {
   return {
     app: {
-      host: env.TEST_APP_HOST || "NOT PROVIDED",
+      host: env.TEST_APP_HOST || "NOT FOUND",
       port: (env.TEST_APP_PORT && parseInt(env.TEST_APP_PORT, 10)) || 404,
     },
     database: {
-      host: env.TEST_DB_HOST || "NOT PROVIDED",
+      host: env.TEST_DB_HOST || "NOT FOUND",
       port: (env.TEST_DB_PORT && parseInt(env.TEST_DB_PORT, 10)) || 404,
-      name: env.TEST_DB_NAME || "NOT PROVIDED",
+      name: env.TEST_DB_NAME || "NOT FOUND",
     },
     secrets: {
-      token: env.TEST_TOKEN_SECRET || "NOT PROVIDED",
-      tokenExp: env.TEST_TOKEN_EXP || "NOT PROVIDED",
+      token: env.TEST_TOKEN_SECRET || "NOT FOUND",
+      tokenExp: env.TEST_TOKEN_EXP || "NOT FOUND",
     },
   };
 };
@@ -39,17 +39,17 @@ const testConfig = (env: IProcessEnv): IEnvConfig => {
 const stageConfig = (env: IProcessEnv): IEnvConfig => {
   return {
     app: {
-      host: env.STAGE_APP_HOST || "NOT PROVIDED",
+      host: env.STAGE_APP_HOST || "NOT FOUND",
       port: (env.STAGE_APP_PORT && parseInt(env.STAGE_APP_PORT, 10)) || 404,
     },
     database: {
-      host: env.STAGE_DB_HOST || "NOT PROVIDED",
+      host: env.STAGE_DB_HOST || "NOT FOUND",
       port: (env.STAGE_DB_PORT && parseInt(env.STAGE_DB_PORT, 10)) || 404,
-      name: env.STAGE_DB_NAME || "NOT PROVIDED",
+      name: env.STAGE_DB_NAME || "NOT FOUND",
     },
     secrets: {
-      token: env.STAGE_TOKEN_SECRET || "NOT PROVIDED",
-      tokenExp: env.STAGE_TOKEN_EXP || "NOT PROVIDED",
+      token: env.STAGE_TOKEN_SECRET || "NOT FOUND",
+      tokenExp: env.STAGE_TOKEN_EXP || "NOT FOUND",
     },
   };
 };
@@ -57,17 +57,17 @@ const stageConfig = (env: IProcessEnv): IEnvConfig => {
 const prodConfig = (env: IProcessEnv): IEnvConfig => {
   return {
     app: {
-      host: env.PROD_APP_HOST || "NOT PROVIDED",
+      host: env.PROD_APP_HOST || "NOT FOUND",
       port: (env.PROD_APP_PORT && parseInt(env.PROD_APP_PORT, 10)) || 404,
     },
     database: {
-      host: env.PROD_DB_HOST || "NOT PROVIDED",
+      host: env.PROD_DB_HOST || "NOT FOUND",
       port: (env.PROD_DB_PORT && parseInt(env.PROD_DB_PORT, 10)) || 404,
-      name: env.PROD_DB_NAME || "NOT PROVIDED",
+      name: env.PROD_DB_NAME || "NOT FOUND",
     },
     secrets: {
-      token: env.PROD_TOKEN_SECRET || "NOT PROVIDED",
-      tokenExp: env.PROD_TOKEN_EXP || "NOT PROVIDED",
+      token: env.PROD_TOKEN_SECRET || "NOT FOUND",
+      tokenExp: env.PROD_TOKEN_EXP || "NOT FOUND",
     },
   };
 };
